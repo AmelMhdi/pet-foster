f048500b89c025ccffee235dd96a9fa5cd6605c4
Sylvie-LD <sylvieleden@gmail.com>
2025-04-28 10:58:22 +0200

    feat: middleware errorHandler, installation of cors and xss-sanitizer

--- src/app.js
+++ src/app.js
@@ -1,10 +1,41 @@
 import "dotenv/config";
 
 import express from "express";
-import  router from "./routers/index.js"
+import router from "./routers/index.js";
+import { notFound, errorHandler } from "./middlewares/errorHandlers.js";
+import cors from "cors";
+import { xss } from "express-xss-sanitizer";
+
 
 // Création de l'app Express
-export const app = express()
+export const app = express();
+
+app.use(xss());
 
 // Brancher le routeur
-app.use(router);
+app.use( router );
+
+app.use(
+  cors({
+    // On définit certains noms de domaines qu'on veut autoriser (certaines origines de notre appel)
+    origin: (origin, callback) => {
+      // Autoriser toutes les origines "localhost" ou "127.0.0.1", peu importe le port
+      if (
+        !origin ||
+                /^(http:\/\/localhost:\d+|http:\/\/127\.0\.0\.1:\d+)$/.test(
+                  origin
+                )
+      ) {
+        callback(null, true); // Autoriser l'origine
+      } else {
+        callback(new Error("Not allowed by CORS")); // Bloquer l'origine
+      }
+    },
+  })
+);
+
+
+
+app.use( notFound );
+
+app.use(errorHandler);
