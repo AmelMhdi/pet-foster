5ce298fab2037562e975b3e0ce0fd6129d3a0a8d
AmelMhdi <amel.m20@gmail.com>
2025-05-05 10:36:29 +0200

    style: modifications

--- pet-foster-connect/src/components/Navbar.tsx
+++ pet-foster-connect/src/components/Navbar.tsx
@@ -3,45 +3,39 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        
-        {/* menu toggler for mobile  */}
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+      {/* menu toggler for mobile  */}
+      <button 
+        className="navbar-toggler ms-auto" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-        {/* collapsible navbar */}
-        <div className="collapse navbar-collapse" id="navbarNav">
-          {/* left nav links */}
+      {/* collapsible navbar */}
+      <div className="collapse navbar-collapse" id="navbarNav">
 
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item mx-2">
-                <Link className="nav-link" to="/">Accueil</Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link className="nav-link" to="/associations">Associations</Link>
-              </li>
-              <li className="nav-item mx-2">
-                <Link className="nav-link" to="/animals">Animaux</Link>
-              </li>
-            </ul>
+        {/* left nav links */}
+        <ul className="navbar-nav me-auto mt-2 mb-2 mb-lg-0">
+            <li className="nav-item mx-2">
+              <Link className="nav-link" to="/">Accueil</Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link className="nav-link" to="/associations">Associations</Link>
+            </li>
+            <li className="nav-item mx-2">
+              <Link className="nav-link" to="/animals">Animaux</Link>
+            </li>
+          </ul>
 
-          {/* right nav links */}
-          <div className="d-flex">
-            <button className="btn btn-light me-2">Connexion</button>
-            <button className="btn custom-inscription-btn">Inscription</button>
-          </div>
-
+        {/* right nav links */}
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-light me-2">Connexion</button>
+          <button className="btn custom-inscription-btn">Inscription</button>
         </div>
-      </div>
 
-    </nav>
+      </div>
     </>
   )
 }
