a73ba67216d63ef681b2178e201f3e57ab51c392
AmelMhdi <amel.m20@gmail.com>
2025-05-14 09:19:38 +0200

    code review

--- pet-foster-connect/src/@types/index.d.ts
+++ pet-foster-connect/src/@types/index.d.ts
@@ -1,34 +1,39 @@
 export type IAnimals = IAnimal[];
 
 export interface IAnimal {
-    id: number;
-    name: string;
-    birthday: string;
-    description: string;
-    picture: string;
-    localisation_id: number;
-    species_id: number;
-    user_id: number;
+  id: number;
+  name: string;
+  birthday: string;
+  description: string;
+  picture: string;
+  localisation_id: number;
+  species_id: number;
+  user_id: number;
+
+  species?: ISpecies;
+  localisation?: ILocalisation;
+  user?: IUser;
 }
+
 export interface ILocalisation {
-    id: number;
-    city: string;
-    postcode: string;
+  id: number;
+  city: string;
+  postcode: string;
 }
+
 export interface IUser {
-    id: number;
-    firstname: string;
-    lastname: string;
-    phone_number: string;
-    address: string;
-    localisation: {
-      city: string,
-      postcode: string
-    };
+  id: number;
+  firstname: string;
+  lastname: string;
+  phone_number: string;
+  address: string;
+
+  localisation?: ILocalisation;
 }
+
 export interface ISpecies {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
 }
 
 export interface IAssociationDetail {
@@ -36,7 +41,6 @@ export interface IAssociationDetail {
   lastname: string;
   email: string;
   phone_number: string;
-  animals_asso: IAnimal[]; 
-  };
 
-
+  animals_asso: IAnimal[];
+};
--- pet-foster-connect/src/components/AssociationDetails.tsx
+++ pet-foster-connect/src/components/AssociationDetails.tsx
@@ -5,21 +5,27 @@ import { fetchAssociationById } from '../services/api';
 import { IAssociationDetail } from '../@types';
 
 export default function ContactAssociation() {
-const { id } = useParams();
- const [association, setAssociation] = useState<IAssociationDetail>();
-const [loading, setLoading] = useState<boolean>(true);
-const [notFound, setNotFound] = useState<boolean>(false);
+  const { id } = useParams();
+  const [association, setAssociation] = useState<IAssociationDetail>();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [notFound, setNotFound] = useState<boolean>(false);
 
-    useEffect(() => {
+  useEffect(() => {
     const loadData = async () => {
-           if (!id) {
-      setNotFound(true);
-      return;
-    }
-
-       try {
-        setLoading(true);
-        const associationId = parseInt(id)
+      if (!id) {
+        setNotFound(true);
+        setLoading(false);
+        return;
+      }
+  
+      const associationId = parseInt(id);
+      if (isNaN(associationId)) {
+        setNotFound(true);
+        setLoading(false);
+        return;
+      }
+  
+      try {
         const oneAssociation = await fetchAssociationById(associationId);
         if (!oneAssociation) {
           setNotFound(true);
@@ -28,34 +34,39 @@ const [notFound, setNotFound] = useState<boolean>(false);
         }
       } catch (error) {
         console.error("Erreur lors du chargement:", error);
-        setNotFound(true); // En cas d'erreur, rediriger aussi vers la page 404
-      }
+        setNotFound(true);
+      } finally {
         setLoading(false);
+      }
     };
+  
     loadData();
+  }, [id]);
+  
+  if(isNaN(Number(id))) {
+    return <Navigate to="/404" replace />;
+  }
 
-}, [id]);
+  if (notFound) {
+  return <Navigate to="/404" replace />;
+  }
 
-if (notFound) {
-return <Navigate to="/404" replace />;
-}
+  if (loading) {
+    return <p>Chargement...</p>;
+  }
 
-if (loading) {
-return <p>Chargement...</p>;
-}
+  if (!association) return <p>Erreur : association non trouvée.</p>;
 
-return (
-<>
-
-<div className="container my-4">
-<div className="mb-4">
-<h2 className="text-center">{association?.lastname}</h2>
-<p className="text-center">
-Contactez-nous au : <span className="fw-bold">{association?.phone_number}</span>{' '}
-ou par mail : <span className="fw-bold">{association?.email}</span>
-</p>
-</div>
-
+  return (
+    <>
+      <div className="container my-4">
+        <div className="mb-4">
+          <h2 className="text-center">{association?.lastname}</h2>
+            <p className="text-center">
+              Contactez-nous au <span className="fw-bold">{association?.phone_number}</span>{' '}
+              ou par mail <span className="fw-bold">{association?.email}</span>
+            </p>
+        </div>
         <div>
           {association?.animals_asso?.length ? (
             <>
@@ -73,9 +84,6 @@ ou par mail : <span className="fw-bold">{association?.email}</span>
                       )}
                       <div className="card-body">
                         <h5 className="card-title">{animal.name}</h5>
-                        <p className="card-text">
-                          Espèce : <span className="fw-bold">{animal.species.name}</span>
-                        </p>
                         <Link 
                           to={`/animals/${animal.id}`} 
                           className="btn btn-outline-primary mt-auto">
@@ -93,7 +101,5 @@ ou par mail : <span className="fw-bold">{association?.email}</span>
         </div>
       </div>
     </>
-
-);
-
+  );
 }
