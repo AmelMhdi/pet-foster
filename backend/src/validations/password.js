e766363f633ba09b5f99236a6c88f543b1ab5c05
Amel Mohammedi <amel.m20@gmail.com>
2025-05-18 12:08:55 +0200

    Password validation file modified to render precise error message

--- backend/src/validations/password.js
+++ backend/src/validations/password.js
@@ -1,18 +1,39 @@
 import Joi from "joi";
 
 export const passwordComplexity = Joi.string()
-  .pattern(/^.{12,}$/, "longueur")
-  .message("Le mot de passe doit contenir au moins 12 caractères")
-  .pattern(/[A-Z]/, "majuscule")
-  .message("Le mot de passe doit contenir au moins une lettre majuscule")
-  .pattern(/[a-z]/, "minuscule")
-  .message("Le mot de passe doit contenir au moins une lettre minuscule")
-  .pattern(/[0-9]/, "chiffre")
-  .message("Le mot de passe doit contenir au moins un chiffre")
-  .pattern(/[\W_]/, "symbole")
-  .message("Le mot de passe doit contenir au moins un symbole")
-  .pattern(/^\S*$/, "pas d'espace")
-  .message("Le mot de passe ne doit pas contenir d'espace")
-  .required();
+  .required()
+  .custom((value, helpers) => {
+    const errors = [];
+
+    if (value.length < 12) {
+      errors.push("au moins 12 caractères");
+    }
+
+    if (!/[A-Z]/.test(value)) {
+      errors.push("au moins une majuscule");
+    }
+
+    if (!/[a-z]/.test(value)) {
+      errors.push("au moins une minuscule");
+    }
+
+    if (!/[0-9]/.test(value)) {
+      errors.push("au moins un chiffre");
+    }
+
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(value)) {
+      errors.push("au moins un caractère spécial");
+    }
+
+    if (/\s/.test(value)) {
+      errors.push("aucun espace");
+    }
+
+    if (errors.length) {
+      return helpers.error("any.invalid", { message: `Le mot de passe doit contenir ${errors.join(", ")}` });
+    }
+
+    return value;
+  }, "Validation de la complexité du mot de passe");
 
 export const updatedPasswordComplexity = passwordComplexity.optional();
