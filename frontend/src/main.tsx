b3e060564c20a3e3475114d024147983b112ba7c
Amel Mohammedi <amel.m20@gmail.com>
2025-05-24 15:00:38 +0200

    Import index css deleted

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 import App from "./App.tsx";
 import './main.css';
-import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
