3f81664c95fe1fef4c1f69bf81c53fff131a2bae
Amel Mohammedi <amel.m20@gmail.com>
2025-06-07 13:57:05 +0200

    Style: homepage presentation text

--- frontend/src/components/MainContainer.tsx
+++ frontend/src/components/MainContainer.tsx
@@ -1,21 +1,22 @@
 export default function MainContainer() {
   return (
-    <main className="container mt-5">
-      <div className="main-container-border row justify-content-center border border-1 rounded p-2">
-        <div className="col-lg-10 text-center">
+    <main className="container my-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-10 main-container-custom text-center">
           <p>
-            Pet Foster Connect révolutionne la protection animale en créant des liens solides entre les associations et les familles d'accueil.
+            <strong>Pet Foster Connect</strong> révolutionne la protection animale en créant des liens solides entre les associations et les familles d'accueil.
           </p>
 
-          <h3 className="section-title fs-4">Notre mission</h3>
+          <h3 className="fs-4">Notre mission</h3>
           <p>
             Offrir une chance à chaque animal en attente d’un foyer définitif. En devenant famille d’accueil, vous transformez des vies — une patte à la fois.
           </p>
 
-          <h3 className="section-title fs-4">Rejoignez notre communauté</h3>
+          <h3 className="fs-4">Rejoignez notre communauté</h3>
           <p>
             Parcourez les profils des animaux, trouvez ceux qui ont besoin de vous et devenez acteur d’un monde plus bienveillant.
-            Pet Foster Connect est plus qu'une application : c'est un mouvement solidaire.
+            <br />
+            <strong>Pet Foster Connect</strong>, c’est plus qu'une application : c'est un mouvement solidaire.
           </p>
         </div>
       </div>
