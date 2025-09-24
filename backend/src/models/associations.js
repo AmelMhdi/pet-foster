b93884c6b459d30d65926c4e59c5f33cdf00224e
AmelMhdi <amel.m20@gmail.com>
2025-09-24 12:34:06 +0200

    feat: sequelize associations redefined

--- backend/src/models/associations.js
+++ backend/src/models/associations.js
@@ -1,98 +1,73 @@
+import { Animal, User } from "./index.js";
 import { sequelize } from "./sequelizeClient.js";
-import { User } from "./User.js";
-import { Role } from "./Role.js";
-import { Animal } from "./Animal.js";
-import { Localisation } from "./Localisation.js";
-import { Species } from "./Species.js";
-import { User_animal } from "./User_animal.js";
 
-Localisation.hasMany(Animal, {
-  as: "animals", 
-  foreignKey: {
-    name: "localisation_id",
-    allowNull: false,
-  },
-  onDelete: "CASCADE"
-});
-Animal.belongsTo(Localisation, {
-  as: "localisation", 
-  foreignKey: "localisation_id" 
-});
-
-Species.hasMany(Animal, {
-  as: "animals",
-  foreignKey: {
-    name: "species_id",
-    allowNull: false,
-  },
-  onDelete: "CASCADE"
-});
-Animal.belongsTo(Species, {
-  as: "species", 
-  foreignKey: "species_id" 
-} );
-
-User.hasMany(Animal, {
-  as: "animals_asso",
-  foreignKey: {
-    name: "user_id",
-    allowNull: false,
-  },
-  onDelete: "CASCADE"
-});
-Animal.belongsTo(User, {
-  as: "asso",
-  foreignKey: "user_id"
-});
-
+// ROLE - USER
 Role.hasMany(User, {
   as: "users",
   foreignKey: {
     name: "role_id",
     allowNull: false,
   },
-  onDelete: "CASCADE"
-});
+  onDelete: "CASCADE",
+})
 User.belongsTo(Role, {
   as: "role",
-  foreignKey: "role_id"
+  foreignKey: "role_id",
 });
 
-User.belongsToMany(Animal, {
-  as: "animals_family",
-  through: "User_animal",
-  foreignKey: "user_id"
-});
-
-Animal.belongsToMany(User, {
-  as: "families",
-  through: "User_animal",
-  foreignKey: "animal_id"
-} );
-
-Localisation.hasMany(User, {
-  as: "users", 
+// USER (as association) - ANIMAL
+User.hasMany(Animal, {
+  as: "animals",
   foreignKey: {
-    name: "localisation_id",
-    allowNull: false
+    name: "user_id",
+    allowNull: false,
   },
-  onDelete: "CASCADE"
-} );
-
-User.belongsTo(Localisation, {
-  as: "localisation", 
-  foreignKey: "localisation_id" 
+  onDelete: "CASCADE",
 });
-
-
-User_animal.belongsTo(User, {
+Animal.belongsTo(User, {
   as: "user",
-  foreignKey: "user_id"
+  foreignKey: "user_id",
 });
 
-User_animal.belongsTo(Animal, {
+// SPECIES - ANIMAL
+Species.hasMany(Animal, {
+  as: "animals",
+  foreignKey: {
+    name: "species_id",
+    allowNull: false,
+  },
+  onDelete: "CASCADE",
+});
+Animal.belongsTo(Species, {
+  as: "species",
+  foreignKey: "species_id",
+});
+
+// APPLICATION (un utilisateur fait plusieurs demandes d'adoption)
+User.hasMany(Application, {
+  as: "applications",
+  foreignKey: {
+    name: "user_id",
+    allowNull: false,
+  },
+  onDelete: "CASCADE",
+});
+Application.belongsTo(User, {
+  as: "user",
+  foreignKey: "user_id",
+});
+
+Animal.hasMany(Application, {
+  as: "applications",
+  foreignKey: {
+    name: "animal_id",
+    allowNull: false,
+  },
+  onDelete: "CASCADE",
+});
+Application.belongsTo(Animal, {
   as: "animal",
-  foreignKey: "animal_id"
+  foreignKey: "animal_id",
 });
 
-export { User, Role, Animal, Species, Localisation, User_animal, sequelize };
+export { Animal, User, Localisation, Species, Role, Application, sequelize };
