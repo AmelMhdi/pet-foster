20ed8a6b4cf88a0a6bbb1e1b9d5d7a409b187bc9
AmelMhdi <amel.m20@gmail.com>
2025-09-24 12:25:34 +0200

    feat: species sequelize model updated

--- backend/src/models/Species.js
+++ backend/src/models/Species.js
@@ -3,13 +3,22 @@ import { sequelize } from "./sequelizeClient.js";
 
 export class Species extends Model {}
 
-Species.init({
+Species.init(
+{
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
   name: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique : true
+    unique : true,
   },
 }, {
   sequelize,
-  tableName: "species"
+  modelName: "Species",
+  tableName: "species",
+  timestamps: true,
+  underscored: true, // Pour que Sequelize utilise le snake_case pour les champs de timestamps (created_at, updated_at)
 });
