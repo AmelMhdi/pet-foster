96808aa8934728774d1700c168a015f599f59271
AmelMhdi <amel.m20@gmail.com>
2025-09-29 12:45:30 +0200

    update: joi schema for user registration

--- backend/src/validations/userSchemas.js
+++ backend/src/validations/userSchemas.js
@@ -4,93 +4,135 @@ import { passwordComplexity, updatedPasswordComplexity } from "./password.js"; /
 
 // Ce schéma décrit ce qu’un nouvel utilisateur doit fournir pour s’inscrire
 export const userRegisterSchema = Joi.object({
-  firstname: Joi.string().min(3).max(30).required().messages({
-    "string.base": "Le prénom doit être une chaîne de caractères",
-    "string.empty": "Le prénom est requis",
-    "string.min": "Le prénom doit contenir au moins 3 caractères",
-    "string.max": "Le prénom doit contenir au maximum 30 caractères",
+  first_name: Joi.string().min(3).max(30).required().messages({
+    "string.base": "Le prénom doit être une chaîne de caractères.",
+    "string.empty": "Le prénom est requis.",
+    "string.min": "Le prénom doit contenir au moins 3 caractères.",
+    "string.max": "Le prénom doit contenir au maximum 30 caractères.",
   }),
-  lastname: Joi.string().min(3).max(30).required().messages({
-    "string.base": "Le nom doit être une chaîne de caractères",
-    "string.empty": "Le nom est requis",
-    "string.min": "Le nom doit contenir au moins 3 caractères",
-    "string.max": "Le nom doit contenir au maximum 30 caractères",
+  last_name: Joi.string().min(3).max(30).required().messages({
+    "string.base": "Le nom doit être une chaîne de caractères.",
+    "string.empty": "Le nom est requis.",
+    "string.min": "Le nom doit contenir au moins 3 caractères.",
+    "string.max": "Le nom doit contenir au maximum 30 caractères.",
+  }),
+  email: Joi.string().email().required().messages({
+    "string.base": "L'email doit être une chaîne de caractères.",
+    "string.empty": "L'email est requis.",
+    "string.email": "L'email doit être une adresse email valide.",
   }),
   password: passwordComplexity, // Validation via passwordComplexity importé
-  email: Joi.string().email().required().messages({
-    "string.base": "L'email doit être une chaîne de caractères",
-    "string.empty": "L'email est requis",
-    "string.email": "L'email doit être une adresse email valide",
-  }),
-  address: Joi.string()
-    .pattern(/^[0-9a-zA-ZÀ-ÿ\s,'-]{3,}$/)
-    .required()
-    .messages({
-      "string.empty": "L'adresse est requise",
-      "string.pattern.base": "L'adresse n'est pas complète",
-    }),
   phone_number: Joi.string()
     .pattern(/^\d{10}$/)
     .required()
     .messages({
-      "string.empty": "Le numéro de téléphone est requis",
+      "string.empty": "Le numéro de téléphone est requis.",
       "string.pattern.base":
-        "Le numéro de téléphone doit être composé de 10 chiffres",
+        "Le numéro de téléphone doit être composé de 10 chiffres. Exemple : 0612345678",
     }),
-  rma_number: Joi.string()
+  street_number: Joi.string()
+    .pattern(/^[0-9]{1,5}$/)
+    .required()
+    .messages({
+      "string.empty": "Le numéro de rue est requis.",
+      "string.pattern.base":
+        "Le numéro de rue doit être un nombre entre 1 et 5 chiffres.",
+    }),
+  address: Joi.string()
+    .pattern(/^[0-9a-zA-ZÀ-ÿ\s,'-]{3,}$/)
+    .required()
+    .messages({
+      "string.empty": "L'adresse est requise.",
+      "string.pattern.base": "L'adresse n'est pas complète.",
+    }),
+  zip_code: Joi.string()
+    .pattern(/^\d{5}$/)
+    .required()
+    .messages({
+      "string.empty": "Le code postal est requis.",
+      "string.pattern.base": "Le code postal doit être composé de 5 chiffres.",
+    }),
+  city: Joi.string()
+    .pattern(/^[a-zA-ZÀ-ÿ\s-]{2,}$/)
+    .required()
+    .messages({
+      "string.empty": "La ville est requise.",
+      "string.pattern.base":
+        "La ville doit contenir au moins 2 caractères et ne peut pas contenir de chiffres ou de caractères spéciaux.",
+    }),
+  rna_number: Joi.string()
     .pattern(/^W\d{9}$/)
     .optional() // Champ optionnel pour associations
     .messages({
       "string.pattern.base":
-        "Le numéro RMA doit commencer par 'W' suivi de 9 chiffres",
-      "string.base": "Le numéro RMA doit être une chaîne de caractères",
-      "any.required": "Le numéro RMA est requis",
+        "Le numéro RNA doit commencer par 'W' suivi de 9 chiffres.",
+      "string.base": "Le numéro RNA doit être une chaîne de caractères.",
+      "any.required": "Le numéro RNA est requis.",
     }),
   role_id: Joi.number().integer().required(), // Entier, obligatoire (lien avec table Role en base)
-  localisation_id: Joi.number().integer().required(), // Entier, obligatoire (lien avec table Localisation en base)
 });
 
 // Même schéma, mais tous les champs deviennent optionnels
 // Il est utilisé quand un utilisateur met à jour son profil et peut modifier un ou plusieurs champs
 export const userUpdateSchema = Joi.object({
-  firstname: Joi.string().min(3).max(30).optional().messages({
-    "string.base": "Le prénom doit être une chaîne de caractères",
-    "string.min": "Le prénom doit contenir au moins 3 caractères",
-    "string.max": "Le prénom doit contenir au maximum 30 caractères",
+  first_name: Joi.string().min(3).max(30).optional().messages({
+    "string.base": "Le prénom doit être une chaîne de caractères.",
+    "string.min": "Le prénom doit contenir au moins 3 caractères.",
+    "string.max": "Le prénom doit contenir au maximum 30 caractères.",
   }),
-  lastname: Joi.string().min(3).max(30).optional().messages({
-    "string.base": "Le nom doit être une chaîne de caractères",
-    "string.min": "Le nom doit contenir au moins 3 caractères",
-    "string.max": "Le nom doit contenir au maximum 30 caractères",
+  last_name: Joi.string().min(3).max(30).optional().messages({
+    "string.base": "Le nom doit être une chaîne de caractères.",
+    "string.min": "Le nom doit contenir au moins 3 caractères.",
+    "string.max": "Le nom doit contenir au maximum 30 caractères.",
+  }),
+  email: Joi.string().email().optional().messages({
+    "string.base": "L'email doit être une chaîne de caractères.",
+    "string.email": "L'email doit être une adresse email valide.",
   }),
   password: updatedPasswordComplexity, // Validation via updatedPasswordComplexity importé
-  email: Joi.string().email().optional().messages({
-    "string.base": "L'email doit être une chaîne de caractères",
-    "string.email": "L'email doit être une adresse email valide",
-  }),
-  address: Joi.string()
-    .pattern(/^[0-9a-zA-ZÀ-ÿ\s,'-]{3,}$/)
-    .optional()
-    .messages({
-      "string.pattern.base": "L'adresse contient des caractères non autorisés",
-    }),
   phone_number: Joi.string()
     .pattern(/^\d{10}$/)
     .optional()
     .messages({
       "string.pattern.base":
-        "Le numéro de téléphone doit être composé de 10 chiffres",
+        "Le numéro de téléphone doit être composé de 10 chiffres.",
     }),
-  rma_number: Joi.string()
+  street_number: Joi.string()
+    .pattern(/^[0-9]{1,5}$/)
+    .required()
+    .messages({
+      "string.empty": "Le numéro de rue est requis.",
+      "string.pattern.base":
+        "Le numéro de rue doit être un nombre entre 1 et 5 chiffres.",
+    }),
+  address: Joi.string()
+    .pattern(/^[0-9a-zA-ZÀ-ÿ\s,'-]{3,}$/)
+    .optional()
+    .messages({
+      "string.pattern.base": "L'adresse contient des caractères non autorisés.",
+    }),
+  zip_code: Joi.string()
+    .pattern(/^\d{5}$/)
+    .optional()
+    .messages({
+      "string.pattern.base": "Le code postal doit être composé de 5 chiffres.",
+    }),
+  city: Joi.string()
+    .pattern(/^[a-zA-ZÀ-ÿ\s-]{2,}$/)
+    .optional()
+    .messages({
+      "string.pattern.base":
+        "La ville doit contenir au moins 2 caractères et ne peut pas contenir de chiffres ou de caractères spéciaux.",
+    }),
+  rna_number: Joi.string()
     .pattern(/^W\d{9}$/)
     .optional()
     .messages({
       "string.pattern.base":
-        "Le numéro RMA doit commencer par 'W' suivi de 9 chiffres",
-      "string.base": "Le numéro RMA doit être une chaîne de caractères",
+        "Le numéro RNA doit commencer par 'W' suivi de 9 chiffres.",
+      "string.base": "Le numéro RNA doit être une chaîne de caractères.",
     }),
-  role_id: Joi.number().integer().optional(),
-  localisation_id: Joi.number().integer().optional(),
+  role_id: Joi.number().integer().optional()
 });
 
 /**
